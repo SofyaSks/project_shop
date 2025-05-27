2539cf12a3c701485ae2fac2c58d9e11180cb7e9
Sof Sur <ssurovneva@mail.ru>
2025-05-27 22:21:07 +0300

    views подгружаю

--- shop/good/views.py
+++ shop/good/views.py
@@ -3,11 +3,31 @@ from . import models
 
 def main(request, uprice):
     context = {
-        'good': models.Good.objects.filter(price = uprice),
+        # 'good': models.Good.objects.filter(price = uprice),
+        'good': models.Good.objects.all(),
         'uprice': uprice
     }
     return render(
         request,
         'good/index.html',
         context      
+    )
+
+from . import forms
+
+def new_good(request):
+    context = {
+        'new_good_post': forms.GoodPostForm()
+    }
+
+    form = forms.GoodPostForm(request.POST)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form.save()
+
+    return render(
+        request,
+        'good/new_good.html',
+        context
+
     )
